74e593a1ade75048337b8d4a8e6cf32f8afabde6
umienatasha <uminatasha22@gmail.com>
2021-11-03 09:11:43 +0800

    Update register.php

--- register.php
+++ register.php
@@ -85,9 +85,9 @@
     // When form submitted, insert values into the database.
     if (isset($_REQUEST['username'])) {
         // removes backslashes
-        $id_patient = stripslashes($_REQUEST['username']);
+        $username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-        $id_patient = mysqli_real_escape_string($conn, $id_patient);
+        $id_patient = mysqli_real_escape_string($conn, $username);
         $email    = stripslashes($_REQUEST['email']);
         $email    = mysqli_real_escape_string($conn, $email);
         $password = stripslashes($_REQUEST['password']);
