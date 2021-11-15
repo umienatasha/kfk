84cd8237559c586c1614305f25e6262280629d9f
umienatasha <uminatasha22@gmail.com>
2021-11-15 22:02:39 +0800

    Update regmaklumat.php

--- regmaklumat.php
+++ regmaklumat.php
@@ -1,6 +1,27 @@
 <?php 
 include('connection.php');
 
+$query = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
+$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+$num_rows = mysqli_num_rows($result);
+if ($num_rows < 2) {
+             $stmt = $mysqli->prepare("INSERT INTO bookings (date, timeslot, name, email, ic, phone, gender, comment, id_patient)  
+			  ('?', '?', '?', '?', '?', '?', '?',  '?', '?')";
+            $msg = "<div class='alert alert-success'>Tempahah Berjaya!
+			
+			<script>
+				window.location='viewbooking.php';
+			</script>
+			
+			</div>";
+        }else {
+            echo "<div class='form'>
+                  <h3>Sudah Penuh !</h3><br/>
+                  <p class='link'>Tekan untuk <a href='timeslot.php'>Tempah</a> tarikh.</p>
+                  </div>";
+        }
+
+
 $date=$_POST['date'];
 $timeslot=$_POST['timeslot'];
 $name=$_POST['name'];
