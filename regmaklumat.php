9a29a5aff57383986d3034628f6d6e7c67d71cf5
umienatasha <uminatasha22@gmail.com>
2021-11-15 20:55:30 +0800

    Update regmaklumat.php

--- regmaklumat.php
+++ regmaklumat.php
@@ -1,8 +1,6 @@
 <?php 
 include('connection.php');
 
-$date=$_POST['date'];
-$timeslot=$_POST['timeslot'];
 $name=$_POST['name'];
 $email=$_POST['email'];
 $ic=$_POST['ic'];
@@ -12,8 +10,8 @@ $address=$_POST['address'];
 $comment=$_POST['comment'];
 
 
-$sql = "INSERT INTO bookings (date, timeslot, name, email, ic, phone, gender, address, comment) 
-		VALUES ('$date', '$timeslot', '$name', '$email', '$ic', '$phone', '$gender', '$address', '$comment')";
+$sql = "INSERT INTO bookings ( name, email, ic, phone, gender, address, comment) 
+		VALUES ( '$name', '$email', '$ic', '$phone', '$gender', '$address', '$comment')";
 
 if ($conn->query($sql) === TRUE) {
 	//echo "New record created successfully";
