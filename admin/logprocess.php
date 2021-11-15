8ebb6d27b5d5096b141a054db42a09c1abcbfc98
umienatasha <uminatasha22@gmail.com>
2021-11-15 16:57:24 +0800

    Update logprocess.php

--- admin/logprocess.php
+++ admin/logprocess.php
@@ -1,36 +1,33 @@
 <?php
 session_start();
-include 'connection.php';
 
-if(isset($_REQUEST['username']) && isset($_REQUEST['password'])){
-
-	//mysqli real escape prevent from sql injection which filter the user input
-	$username=mysqli_real_escape_string($conn,$_REQUEST['username']);
-	$password=mysqli_real_escape_string($conn,$_REQUEST['password']);
-	$qr=mysqli_query($conn,"SELECT * FROM loginuser where username='".$username."' and password='".($password)."'");
-	if(mysqli_num_rows($qr)>0){
-		while($data=mysqli_fetch_assoc($qr))
-                {
-		if($data['user_type']=='admin'){
-                    $_SESSION['idadmin']=$data["id"];
-                    echo '<script type="text/javascript">
-                      alert("Welcome Admin!");
-			location="admin.php";
-                        </script>';
-		}
-		elseif ($data['user_type']=='patient'){
-                    $_SESSION['idinstructor']=$data["id"];
-                    echo '<script type="text/javascript">
-                      alert("Welcome Patient!");
-			location="patient/index.php";
-                        </script>';
-		}
-	}
-            }
-	else{
-		echo '<script type="text/javascript">
-                      alert("Invalid username or passsword. Please try again.");
-			location="login.php";
-                        </script>';
-	}
+$conn = mysqli_connect('localhost', 'root' );
+if($conn){
+	echo "connection successful";
+}else{
+	echo "no connection";
 }
+
+$db = mysqli_select_db($conn, 'ohana');
+
+if(isset($_POST['submit'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$sql = " select * from  admin where username='$username' and password='$password' ";
+	$query = mysqli_query($conn,$sql);
+
+	$row = mysqli_num_rows($query);
+		if($row == 1){
+			echo "login successful";
+			$_SESSION['username'] = $username;
+			header('location:admin.php');
+		}else{
+			echo "login failed";
+			header('location:login.php');
+		}
+
+}
+
+
+?>
