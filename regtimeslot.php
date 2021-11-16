94e3a55e2b37c72be92b11ba6a69f46211e7cd18
umienatasha <uminatasha22@gmail.com>
2021-11-16 21:24:20 +0800

    Update regtimeslot.php

--- regtimeslot.php
+++ regtimeslot.php
@@ -9,7 +9,7 @@ $query = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot'
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $num_rows = mysqli_num_rows($result);
 if ($num_rows < 2) {
-	$sql = "INSERT INTO bookings (date, timeslot) VALUES ('$date', '$timeslot')";
+	$sql = "SELECT INTO bookings (date, timeslot) VALUES ('$date', '$timeslot')";
 	echo '<script type="text/javascript">
                       alert("Tarikh Kekosongan !");
 			location="maklumat.php";
