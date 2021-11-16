1a8d922a23ad9090e1fd3e304e1eb215bd9bc234
umienatasha <uminatasha22@gmail.com>
2021-11-16 15:05:21 +0800

    Update regtimeslot.php

--- regtimeslot.php
+++ regtimeslot.php
@@ -1,25 +1,23 @@
-<?php 
+<?php
 include('connection.php');
 
 $date=$_POST['date'];
 $timeslot=$_POST['timeslot'];
+$id_patient=$_SESSION['id_patient'];
 
-
-$sql = "INSERT INTO bookings (date, timeslot) 
-		VALUES ('$date', '$timeslot')";
-
-if ($conn->query($sql) === TRUE) {
-	//echo "New record created successfully";
-}
-else {
-	echo "Error: " .$sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+$query = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
+$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+$num_rows = mysqli_num_rows($result);
+if ($num_rows < 2) {
+	$sql = "INSERT INTO bookings (date, timeslot) VALUES ('$date', '$timeslot')";
+	echo '<script type="text/javascript">
+                      alert("Tarikh Kekosongan !");
+			location="maklumat.php";
+                        </script>';
+   } else{
+		echo '<script type="text/javascript">
+                      alert("Tempahan Sudah Penuh !");
+			location="timeslot.php";
+                        </script>';
+	}
 ?>
-
-<script>
-
-	window.location="maklumat.php";
-
-</script>
