fec2c275fe92cc6a3ee1f63bf2cea2c817b6e271
umienatasha <uminatasha22@gmail.com>
2021-11-17 15:32:48 +0800

    Update regtimeslot.php

--- regtimeslot.php
+++ regtimeslot.php
@@ -8,20 +8,9 @@ $id_patient=$_SESSION['id_patient'];
 $sql = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 $num_rows = mysqli_num_rows($result);
-if ($num_rows->query($sql) < 2) {
-	?>
-	echo '<script type="text/javascript">
-                      alert("Tarikh Kekosongan !");
-			location="maklumat.php?date=<?php echo $date ;?>&timeslot=<?php echo $timeslot ;?>";
-			
-                        </script>';
-   } else{
-		echo '<script type="text/javascript">
-                      alert("Tempahan Sudah Penuh !");
-			location="timeslot.php";
-                        </script>';
-	}
-
-$conn->close();	
-?>
+if ($num_rows < 2) 
+ ?>
+ <script>
+				window.location="maklumat.php?date=<?php echo $date ;?>&timeslot=<?php echo $timeslot ;?>";
+ </script>
 
