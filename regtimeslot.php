fc374f9b68b0f5060aae82a03bf0ee87e1dc5139
umienatasha <uminatasha22@gmail.com>
2021-11-17 09:41:45 +0800

    Update regtimeslot.php

--- regtimeslot.php
+++ regtimeslot.php
@@ -5,7 +5,7 @@ $date=$_POST['date'];
 $timeslot=$_POST['timeslot'];
 $id_patient=$_SESSION['id_patient'];
 
-$sql = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
+$sql = "INSERT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 $num_rows = mysqli_num_rows($result);
 if ($num_rows < 2) {
