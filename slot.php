60f2892c1de88bcc1fe1ac587430b8683544282c
umienatasha <uminatasha22@gmail.com>
2021-11-17 20:33:48 +0800

    Create slot.php

---
+++ slot.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+	<title>Pilih Slot</title>
+	<head>
+		<style>
+		table {
+		  font-family: arial, sans-serif;
+		  border-collapse: collapse;
+		  width: 100%;
+		}
+
+		td, th {
+		  border: 1px solid #dddddd;
+		  text-align: left;
+		  padding: 8px;
+		}
+
+		tr:nth-child(even) {
+		  background-color: #dddddd;
+		}
+		</style>
+	</head>
+	<body>
+
+		<h2>Appointment Booking</h2>
+
+		<table>
+		  <tr>
+			<th>Hari</th>
+			<th>Tarikh</th>
+			<th>09:00</th>
+			<th>10:00</th>
+			<th>11:00</th>
+			<th>12:00</th>
+			<th>02:00</th>
+			<th>03:00</th>
+			<th>04:00</th>
+			<th>05:00</th>
+		  </tr>
+		  <tr>
+			<td>Khamis</td>
+			<td>18.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		  <tr>
+			<td>Jumaat</td>
+			<td>19.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		  <tr>
+			<td>Sabtu</td>
+			<td>20.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		  <tr>
+			<td>Ahad</td>
+			<td>21.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		  <tr>
+			<td>Isnin</td>
+			<td>22.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		  <tr>
+			<td>Selasa</td>
+			<td>23.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		</table>
+
+	</body>
+</html>
