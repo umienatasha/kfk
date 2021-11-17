39b5aee41efed4fe1f4772831d69f8ac79bbae2f
umienatasha <uminatasha22@gmail.com>
2021-11-17 09:35:00 +0800

    Update regtimeslot.php

--- regtimeslot.php
+++ regtimeslot.php
@@ -5,11 +5,12 @@ $date=$_POST['date'];
 $timeslot=$_POST['timeslot'];
 $id_patient=$_SESSION['id_patient'];
 
-$query = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
-$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+$sql = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
+$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 $num_rows = mysqli_num_rows($result);
 if ($num_rows < 2) {
-	$sql = "SELECT INTO bookings (date, timeslot) VALUES ('$date', '$timeslot')";
+	$query = "SELECT * FROM bookings WHERE date='$date'
+                     AND timeslot='$timeslot'";
 	?>
 	
 	echo '<script type="text/javascript">
