158a978ad9abbb7661ec933650d657609251feb2
umienatasha <uminatasha22@gmail.com>
2021-11-18 12:49:07 +0800

    Update slot.php

--- slot.php
+++ slot.php
@@ -1,3 +1,20 @@
+<?php
+include('connection.php');
+ if(!isset($_SESSION["id_patient"])) {
+        //  header("Location: login.php");
+		print_r($_SESSION);
+        exit();
+    }
+	
+$id_patient=$_SESSION['id_patient'];
+$sql = "SELECT  * FROM tblpatient WHERE id_patient='$id_patient' ";
+$result = mysqli_query($conn, $sql);
+
+$row = mysqli_fetch_assoc($result); // cara declare row
+$username=$row['username'];
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<title>Pilih Slot</title>
@@ -21,80 +38,31 @@
 		</style>
 	</head>
 	<body>
-
+		
+		<center><h1 class="my-5">Hey, <?php echo $username; ?>!</b>.</h1></center>
+		
 		<h2>Appointment Booking</h2>
+	<form method="post" action="">
 
 		<table>
 		  <tr>
 			<th>Hari</th>
 			<th>Tarikh</th>
-			<th>09:00</th>
-			<th>10:00</th>
-			<th>11:00</th>
-			<th>12:00</th>
-			<th>02:00</th>
-			<th>03:00</th>
-			<th>04:00</th>
-			<th>05:00</th>
+			<th>09:00 am</th>
+			<th>10:00 am</th>
+			<th>11:00 am</th>
+			<th>12:00 pm</th>
+			<th>02:00 pm</th>
+			<th>03:00 pm</th>
+			<th>04:00 pm</th>
+			<th>05:00 pm</th>
 		  </tr>
 		  
-		  <tr>
-			<td>Khamis</td>
-			<td>18.11.21</td>
-			<td name="timeslot"><a href="maklumat.php?date=<?php echo $date ;?>&timeslot=<?php echo $timeslot ;?>">Book</a></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-		  </tr>
-		  
-		  <tr>
-			<td>Jumaat</td>
-			<td>19.11.21</td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-		  </tr>
-		  
-		  <tr>
-			<td>Sabtu</td>
-			<td>20.11.21</td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-		  </tr>
-		  
-		  <tr>
-			<td>Ahad</td>
-			<td>21.11.21</td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-			<td style="background-color:#ff0000;"></td>
-		  </tr>
-		  
-		  <tr>
+		  <tr id="book">
 			<td>Isnin</td>
-			<td>22.11.21</td>
-			<td></td>
-			<td></td>
+			<td name="date">22.11.21</td>
+			<td name="timeslot"><a href="regtimeslot.php">Tempah</a></td>
+			<td>Tempah</td>
 			<td></td>
 			<td></td>
 			<td></td>
@@ -115,7 +83,74 @@
 			<td></td>
 			<td></td>
 		  </tr>
+		  
+		  <tr>
+			<td>Rabu</td>
+			<td>24.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		  
+		  <tr>
+			<td>Khamis</td>
+			<td>25.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		  
+		  <tr>
+			<td>Jumaat</td>
+			<td>26.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		  </tr>
+		  
+		  <tr>
+			<td>Sabtu</td>
+			<td>27.11.21</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+		  </tr>
+		  
+		  <tr>
+			<td>Ahad</td>
+			<td>28.11.21</td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+			<td style="background-color:#ff0000;"></td>
+		  </tr>
 		</table>
+		
+	</form>
 
 	</body>
 </html>
